1239cbdb23fbda0fbd9ed2d24b4b430e382d817f
semantic-release <semantic-release>
2025-10-28 14:09:35 +0000

    1.0.0
    Automatically generated by python-semantic-release

--- django_monaco_editor/__init__.py
+++ django_monaco_editor/__init__.py
@@ -2,7 +2,7 @@
 Django Monaco Editor - Monaco Editor integration for Django forms and admin.
 """
 
-__version__ = '0.0.1'
+__version__ = '1.0.0'
 
 from .fields import MonacoField
 from .widgets import MonacoEditorWidget
